6241cc34d1005abc735010ff62ff88a9ceef5a28
fferraro <ferraro@cs.jhu.edu>
2014-06-17 14:08:36 -0400

    added republish script

---
+++ republish.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -o nounset
+
+DB="db.sqlite3"
+TEMPLATE="$1"
+DATA="$2"
+
+ps aux | grep -i 'python manage.py' | grep -v grep | cut -f2 -d' ' | xargs -I{} kill -9 {}
+
+if [[ -e "$DB" ]];
+    then rm "$DB"
+fi
+
+python manage.py syncdb --noinput
+
+python manage.py publish_hits "$TEMPLATE" "$DATA"
+
+python manage.py runserver 
+
+
+
